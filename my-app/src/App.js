7d78a4e013179e01e6d36288c5ff5c4dc1178441
John Gaines <Firefly4266@users.noreply.github.com>
2019-01-22 21:56:40 -0800

    used terminal to generate react app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <h1>Reaction Time!</h1>
+          <p> Welcome hommies...what's good!?? </p>
+      </div>
+    );
+  }
+}
+
+export default App;
