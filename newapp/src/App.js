7e6d1ce87912e91d7cfaece6c6d1f2d8d8fda045
John Gaines <Firefly4266@users.noreply.github.com>
2019-02-03 14:36:03 -0800

    Added a form to add more tasks

--- newapp/src/App.js
+++ newapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddForm from './AddForm';
 
 class App extends Component {
   state = {
@@ -7,6 +8,15 @@ class App extends Component {
       { id: 1, content: 'Feed the Tree' },
       { id: 2, content: 'Adopt more Trees' }
     ]
+  }
+
+  addTask = (todo)=>{
+    todo.id = Math.random();
+    //use spread operator to drop in the new todo
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
   }
 
   completeTodo = (id)=>{
@@ -23,6 +33,7 @@ class App extends Component {
       <div className="App container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos completeTodo= { this.completeTodo } todos={ this.state.todos } />
+        <AddForm addTask= {this.addTask} />
       </div>
     );
   }
