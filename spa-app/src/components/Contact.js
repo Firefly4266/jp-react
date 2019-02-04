8d5bf7bdaddcc2fbd75b9cfe8aba28cdbada5616
John Gaines <Firefly4266@users.noreply.github.com>
2019-02-03 16:22:38 -0800

    created new app, added pages but no router yet

---
+++ spa-app/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = ()=>{
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eius iure distinctio minus, esse quis! Recusandae debitis, sed molestias a ex porro deserunt facere quos, necessitatibus impedit dignissimos vel. Explicabo.</p>
+        </div> 
+    )
+}
+
+export default Contact
