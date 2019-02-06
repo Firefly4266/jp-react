1d8a6123ace330af377b5440bbe196d415bbc034
John Gaines <Firefly4266@users.noreply.github.com>
2019-02-05 20:45:12 -0800

    added redirect

--- spa-app/src/components/Navbar.js
+++ spa-app/src/components/Navbar.js
@@ -1,18 +1,29 @@
 import React from 'react';
+/* #1 the <NavLink> and <Link> tags are part of the react-router-dom. use them 
+ instead of the <a> tag and they act like preventDefault() does in a form. */
+import {NavLink, Link, withRouter } from 'react-router-dom'; // #1
 
-const Navbar = ()=>{
+/* #2 pass the props to the navbar. the props have the history object with the push 
+  method attached to it. use the push to "push" (redirect) us to a different page */
+
+const Navbar = (props)=>{ // #2 
+    setTimeout(()=>{
+        props.history.push('/')
+    }, 5000);
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
                 <a href='' className="brand-logo">Spa Application</a>
                 <ul className='right'>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
                 </ul>
             </div>
         </nav>
     )
 } 
 
-export default Navbar
+/* withRouter is a Higher Order Component (HOC) that will wrap the navbar with router 
+information like props. This HOC comes with the react-router-dom */
+export default withRouter(Navbar)
